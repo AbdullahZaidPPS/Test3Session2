2f3f5cb770bfea453afdf85eec9f83297ea8a1b9
Abdullah M Zaid <95867207+AbdullahZaidPPS@users.noreply.github.com>
2022-03-03 08:49:40 +0000

    p7

--- p7final.c
+++ p7final.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+struct _fraction
+{
+  int num,den;
+};
+typedef struct _fraction Fraction;
+int find_gcd(int a,int b)
+{
+  
+  int t=1;
+    while (b!=0)
+      {
+        t = b;
+        b = a%b;
+        a = t;
+        } 
+    return a;
+}
+int main()
+{
+  printf("%d\n",find_gcd(4,8));
+  return 0;
+}
 
